58b71a56a2b38391ffcad838ff2b916d49f326f9
magreencodes <122040011+magreencodes@users.noreply.github.com>
2023-01-10 00:58:23 -0500

    Update wordsearch_sprint1.py

--- wordsearch_sprint1.py
+++ wordsearch_sprint1.py
@@ -1,81 +1,112 @@
 #using these later on with random.choice(string.ascii_uppercase)
-import string
+#clear output function when the user finds a word- clear()
+
+import string 
 import random
 
 grid = []
 
-# def setup():
-# 	for i in range(10):
-# 		grid.append([])
-# 		for j in range(11):
-# 			grid[i].append('*')
+#later on in the menu the user can selected the dimensions
+width = 11
+height = 10 
 
-# def display():
-# 	for row in grid:
-# 		for col in row:
-# 			print(col," ", end= "" )
-# 		print()
+def setup():
+	for i in range(height):
+		grid.append([])
+		for j in range(width):
+			grid[i].append('*')
 
-# setup()
+def display():
+	for row in grid:
+		for col in row:
+			print(col," ", end= "" )
+		print()
+
+setup()
 
 
-#Original Place word
 # def place_word(word):
 # 	for i in range(len(word)):
-# 		grid[0][i] = word[::-1][i]
-
-#ERROR HERE- working on randomizing the placement of the word now
-# def place_word(word):
-# 	for i in range(len(word)):
-# 		word = random.choice(word, word[::-1][i])
-# 		format= [grid[i][0], grid[0][i], grid[i][i]]
-# 		random.choice(tuple(format)) = word # [1,0]- going to the right, [0,1]- going down, [1,1]- going diagonally
+# 		for row in grid:
+# 			if row - len(word) < 0: 
+# 				grid[random.randrange(row)][i] = word[::-1][i]
+# 			else:
+# 				False 
 
 # place_word("cat")
-# display()
+
+display()
+
+def place_word(word):
+	rand_num= [1,2,3,4]
+	rand_num = random.choice(directions)
+	if directions == 1:
+		for i in range(height):
+			if height-len(word) > 0:
+				grid[0][1] = word[i]
+	if directions == 2:
+		for i in range(height):
+			if height-len(word) > 0:
+				grid[0][1] = word[::-1][i]
+	if directions == 3:
+		for i in range(width):
+			if width-len(word) > 0:
+				grid[1][0] = word[i]
+		if directions == 4:
+				for i in range(width):
+					if width-len(word) > 0:
+						grid[1][0] = word[::-1][i]
+						
+						
+
+place_word('dog')
 
 
-class Board:
-	def __init__(self, height=10, width=11, word):
-		# self.grid = grid
-		self.height = height
-		self.width = width
-		self.word = word
+ # [1,1]- going diagonally 
 
-	def create_grid(self):
-		for i in range(self.height):
-			grid.append([])
-			for j in range(self.width):
-				grid[i].append('*')
 
-	def display(self, grid):
-		# self.grid = self.height*self.width
-		for self.width in grid:
-			for self.height in self.width:
-				print(self.height, " ", end=" ")
-			print()
+# class Board:
+# 	def __init__(self, grid, height=10, width=11):
+# 		self.height = height 
+# 		self.width = width
+# 		self.grid = []
+# 		self.create_grid()
+		
+# 	def create_grid(self):
+# 		for i in range(self.height):
+# 			self.grid.append([])
+# 			for j in range(self.width):
+# 				self.grid[i].append('*')
+		
+# 	def display(self):
+# 		# self.grid = self.height*self.width
+# 		for row in self.grid:
+# 			for col in row:
+# 				print(col," ", end= "" )
+# 			print()
 
-	#WORK IN PROGRESS
+		#def place_word(word):
+			# for i in range(len(word)):
+		# rowsize= self.width - len(word)
+		# colsize= self.height - len(word)
+
+	#WORK IN PROGRESS 
 	# def fit_word(self, word):
-	# 	if direction[0] == 0: #means that the word is not moving to the right if true then that's the width
+	# 	if direction[0] == 0: #means that the word is not moving to the right if true then that's the width 
 	# 	else:
 	# 		self.width - len(word)
+				
 
-
-
+		
 def main1():
 
-	animals = Board(10, 11, 'cat')
-	print(animals.height, animals.width, animals.word)
+	animals = Board(10, 11)
+	print(animals.height, animals.width,)
 
 	# history.create_grid()
 	# history.display()
 
+	
 
+# main1()
 
-main1()
-
-
-
-#know where the words are- starting point and direction- later on when you check ud they got all of the words
-#idea for later: list of where the words are on the grid check to see for word a boolean- need to store
